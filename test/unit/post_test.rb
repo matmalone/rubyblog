ba9ca6d966ef21a618c339a24b6dcc33933d7829
Mat Malone <mmalone@datacare.com>
2011-10-04 21:17:30 -0700

    unit tests done

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "post won't save with a title only" do
+    post = Post.new
+    post.title = 'test title'
+    assert !post.save, "Saved posted without a name"
+  end
+
+  test "post won't save with a name only" do
+    post = Post.new
+    post.name = 'test name'
+    assert !post.save, "Saved post without a title"
+  end
+
+  test "post will save with a name and a title" do
+    post = Post.new
+    post.name = 'test name'
+    post.title = 'test title'
+    assert post.save
+  end
 end
