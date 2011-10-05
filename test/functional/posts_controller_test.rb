4c12402f9d99222bbeb24f0b37d352c8f5acf642
Mat Malone <mmalone@datacare.com>
2011-10-04 23:40:29 -0700

    edit input form value substring test

--- test/functional/posts_controller_test.rb
+++ test/functional/posts_controller_test.rb
@@ -16,6 +16,15 @@ class PostsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:posts)
   end
 
+  test "should get index view" do
+    get :index
+    
+    assert_select 'title', /Index/
+    assert_select 'td', /My Name \d/, 2
+    assert_select 'td', /My Title \d/, 2
+    assert_select 'td', /My Content \d/, 2
+  end
+
   test "should get new" do
     get :new
     assert_response :success
@@ -23,33 +32,52 @@ class PostsControllerTest < ActionController::TestCase
 
   test "should create post" do
     assert_difference('Post.count') do
-      post :create, :post => @post.attributes
+      post :create, :post => { :name => 'some dude', :title => 'Hi there', :content => 'This is my first post.'}
     end
-
+  
     assert_redirected_to post_path(assigns(:post))
 
     assert_match(/successfully created/, flash[:notice])
   end
 
-  test "should not create post with short title" do
+  test "should not create post with bad args" do
     assert_response :success
 
+    # no name
     assert_difference('Post.count', 0) do
-      attr = @post.attributes
-      attr[:title] = 't'
-      post :create, :post => attr
+      post :create, :post => { :title => 'Hi', :content => 'This is my first post.'}
     end
 
+    # short title
+    assert_difference('Post.count', 0) do
+      post :create, :post => { :name => 'some dude', :title => 'Hi', :content => 'This is my first post.'}
+    end
   end
 
   test "should show post" do
     get :show, :id => @post.to_param
+
     assert_response :success
+  end
+  
+  test "should show post view" do
+    get :show, :id => @post.to_param
+    
+    assert_select 'title', /My Title/
+    assert_select 'p', /My Name/
+    assert_select 'p', /My Title/
+    assert_select 'p', /My Content/    
   end
 
   test "should get edit" do
     get :edit, :id => @post.to_param
     assert_response :success
+  end
+
+  test "should get edit view" do
+    get :edit, :id => @post.to_param
+    assert_select "#post_name[type=text][value*=My Name]"
+
   end
 
   test "should update post" do
