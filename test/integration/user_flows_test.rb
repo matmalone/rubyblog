2cf45591eaccb8e7b8e973b2a70448b13db00f30
Mat Malone <mmalone@datacare.com>
2011-10-09 15:41:19 -0700

    more tests, fixed, really

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -2,12 +2,25 @@ require 'test_helper'
 
 class UserFlowsTest < ActionDispatch::IntegrationTest
   fixtures :all
+  #include Devise::TestHelpers
 
+#  setup do
+#    sign_in users(:user1)
+#  end
+  
   test "view home" do
     get "/"
     assert_response :success
 
     assert_select 'h1', /Hello, World/
-    assert_select 'a[href=/posts]', /My Blog/
+    assert https!
+    assert_select 'a', /My Blog/
+  end
+
+  test "login" do
+    #post_via_redirect '/users/sign-in', :user_email =>
+    #users(:user2).email, :user_password => users(:user2).password
+    #get_sign_in_page
+    
   end
 end
